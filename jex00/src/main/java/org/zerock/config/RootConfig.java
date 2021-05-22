4e98d914c786daa8ab5318953052bc4e859b4fce
jin_crates <94jingyu@gmail.com>
2021-05-22 20:03:52 +0900

    스프링과 MySQL Database 연동

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -1,11 +1,29 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 // 해당 클래스의 인스턴스를 이용해서 설정 파일을 대신함
 @Configuration
 @ComponentScan(basePackages= {"org.zerock.sample"})
 public class RootConfig {
 
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/mydb?serverTimezone=UTC");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("1q2w3e4r!");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
 }
--- jex00/src/test/java/org/zerock/sample/SampleTests.java
+++ jex00/src/test/java/org/zerock/sample/SampleTests.java
@@ -7,22 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.config.RootConfig;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-// 1) @ContextConfiguration
-//    - 지정된 클래스나 문자열을 이용해서 필요한 객체들을 스프링 내 객체로 등록함
-//    - 이를 스프링의 빈(Bean)으로 등록된다고 표현
-// 2) @Log4j
-//    - Lombok을 이용해서 로그를 기록하는 Logger를 변수로 생성함
-//    - 로그에 대한 설정은 'src/main/resources'와 'src/test/resources'에 별도로 존재
-// 3) @Autowired
-//    - 해당 인스턴스 변수가 스프링으로부터 자동으로 주입해 달라는 표시
-// 4) @Test
-//    - JUnit에서 테스트 대상을 표시하는 어노테이션
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration(classes = {RootConfig.class})
 @Log4j
 public class SampleTests {
 	
