5a2edb988579efd9f547bd2c6c80ce3eb1e83594
jin_crates <94jingyu@gmail.com>
2021-05-22 11:40:30 +0900

    SampleTests 클래스 생성

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -1,9 +1,11 @@
 package org.zerock.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 // 해당 클래스의 인스턴스를 이용해서 설정 파일을 대신함
 @Configuration
+@ComponentScan(basePackages= {"org.zerock.sample"})
 public class RootConfig {
 
 }
