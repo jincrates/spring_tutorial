a0a945261ffdac3456f4a79cb2d483f5a35d29a9
jin_crates <94jingyu@gmail.com>
2021-05-28 23:19:51 +0900

    객체 타입

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,17 +1,15 @@
 package org.zerock.controller;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -92,5 +90,39 @@ public class SampleController {
 		return "ex03";
 	}
 	
+	// @ModelAttribute: 강제로 전달받은 파라미터를 Model에 담아서 전달할 때 사용
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto: " + dto);
+		log.info("page: " + page);
+		
+		return "/sample/ex04";
+	}
+	
+	/* 
+	 Controller의 리턴 타입
+	 - String: jsp를 이용하는 경우에는 jsp 파일의 경로와 파일이름을 나타내기 위해 사용
+	 - void: 호출하는 URL과 동일한 이름의 jsp를 의미
+	 - VO, DTO 타입: 주로 JSON 타입의 데이터를 만들어서 반환하는 용도로 사용
+	 - ResponseEntity 타입: response할 떄 Http 헤더 정보의 내용을 가공하는 용도로 사용
+	 - Model, ModelAndView: Model로 데이터를 반환하거나 화면까지 같이 지정하는 경우에 사용(최근에는 많이 사용하지 않음)
+	 - HttpHeaders: 응답에 내용없이 Http 헤더 메시지만 전달하는 용도로 사용
+	 */
+	
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("/ex05..............");
+	}
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06................");
+		
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		
+		return dto;
+	}
 	
 }
---
+++ ex01/src/main/webapp/WEB-INF/views/sample/ex04.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h2>SAMPLE ${sampleDTO }</h2>
+	<h2>PAGE ${page }</h2>
+	<!-- http://localhost:8081/sample/ex04?name=aaa&age=11&page=9 -->
+</body>
+</html>
