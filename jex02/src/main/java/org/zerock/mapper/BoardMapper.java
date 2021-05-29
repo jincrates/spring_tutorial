cde9129c2e7e01c262fd4c334999559ddaedbd41
jin_crates <94jingyu@gmail.com>
2021-05-29 16:35:44 +0900

    영속/비즈니스 계층의 CRUD 구현

--- jex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ jex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -10,4 +10,14 @@ public interface BoardMapper {
 	// 'WHERE BNO > 0'과 같은 조건은 테이블을 검색할 때 PK를 이용하도록 유도하는 조건
 	// @Select("SELECT * FROM TBL_BOARD WHERE BNO > 0")
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
 }
--- jex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ jex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,10 +1,12 @@
 package org.zerock.mapper;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -17,8 +19,50 @@ public class BoardMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
 	
-	@Test
+	@Ignore
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Ignore
+	public void testInsert() {
+		BoardVO board = new  BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		
+		//mapper.insert(board);
+		mapper.insertSelectKey(board);
+		
+		// Lombok이 만들어주는 toString()을 이용해서 bno 멤버 변수의 값을 알아보기 위함
+		log.info(board);
+	}
+	
+	@Ignore
+	public void testRead() {
+		
+		// 존재하는 게시물 번호로 테스트 
+		BoardVO board = mapper.read(7L);
+		
+		log.info(board);
+	}
+	
+	@Ignore
+	public void testDelete() {
+		log.info("DELETE COUNT: " + mapper.delete(3L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		
+		// 실행전 존재하는 번호인지 확인할 것
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
 	}
 }
