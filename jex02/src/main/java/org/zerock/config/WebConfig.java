3c2fe93c90a66c746feddefb3b15ee369fe48032
jin_crates <94jingyu@gmail.com>
2021-06-02 23:21:33 +0900

    한글 문제와 UTF-8 필터 처리

--- jex02/src/main/java/org/zerock/config/WebConfig.java
+++ jex02/src/main/java/org/zerock/config/WebConfig.java
@@ -1,7 +1,9 @@
 package org.zerock.config;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -24,6 +26,15 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 	@Override
 	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
 		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter chracterEncodingFilter = new CharacterEncodingFilter();
+		chracterEncodingFilter.setEncoding("UTF-8");
+		chracterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { chracterEncodingFilter };
 	}
 
 }
