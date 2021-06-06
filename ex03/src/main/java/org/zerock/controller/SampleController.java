32364de994851038821f91ea0d0d5246052e238d
jin_crates <94jingyu@gmail.com>
2021-06-06 11:50:02 +0900

    AJAX Controller 테스트

---
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,55 @@
+package org.zerock.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
+
+import lombok.extern.log4j.Log4j;
+
+// @RestController는 JSP와 달리 순수한 데이터를 반환하는 형태이므로 다양한 포맷의 데이터를 전송할 수 있다.
+@RestController
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+	
+	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+	public String getText() {
+		
+		log.info("MIME TYPE:" + MediaType.TEXT_PLAIN_VALUE);
+		
+		return "안녕하세요.";
+		
+	}
+	
+	@GetMapping(value = "/getSample", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public SampleVO getSample() {
+		return new SampleVO(112, "스타", "로드");
+	}
+	
+	@GetMapping(value = "/getSample2")
+	public SampleVO getSample2() {
+		return new SampleVO(113, "로켓", "라쿤");
+	}
+	
+	@GetMapping(value = "/getList")
+	public List<SampleVO> getList() {
+		return IntStream.range(1, 10).mapToObj(i -> new SampleVO(i, i + " First", i + " Last")).collect(Collectors.toList());
+	}
+	
+	// Map을 이용하는 경우에는 '키(key)'에 속하는 데이터는 XML로 변환되는 경우에 태그의 이름이 됨
+	@GetMapping(value = "/getMap")
+	public Map<String, SampleVO> getMap() {
+		Map<String, SampleVO> map = new HashMap<>();
+		map.put("First", new SampleVO(111, "그루트", "주니어"));
+		
+		return map;
+	}
+}
